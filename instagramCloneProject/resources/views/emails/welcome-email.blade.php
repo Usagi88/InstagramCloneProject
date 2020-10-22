74feef05b9a66c6c6e564bdd17f7a26438974442
Usagi88 <ibrahimnaish5677@gmail.com>
2020-10-22 15:17:18 +0500

    first commit

---
+++ instagramCloneProject/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome to fake Instagram Clone
+
+This is a fake website made for practicing Laravel. Thank you for joining.
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+All the best, <br>
+Tester
+@endcomponent
